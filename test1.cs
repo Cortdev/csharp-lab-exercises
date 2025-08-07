82644e5dfb14fe096df8d09fa8428f30c561a711
Cortezano <johnpatrickcortezano2@gmail.comgit config user.email>
2025-08-07 13:28:29 +0800

    first commit

--- test1.cs
+++ test1.cs
@@ -1,25 +1,25 @@
-    //  using System;      
-    // namespace Test1
-    // {
-    //     class Program
-    //     {
-    //     static void Main(string[] args)
-    //     {
+     using System;      
+    namespace Test1
+    {
+        class Program
+        {
+        static void Main(string[] args)
+        {
             
-    //     int num1, num2, result; 
-    //     Console.WriteLine("Enter a first number:");
-    //     num1 = Convert.ToInt32(Console.ReadLine()); 
-    //     Console.WriteLine("Enter a second number:");
-    //     num2 = Convert.ToInt32(Console.ReadLine()); 
+        int num1, num2, result; 
+        Console.WriteLine("Enter a first number:");
+        num1 = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Enter a second number:");
+        num2 = Convert.ToInt32(Console.ReadLine()); 
     
-    //     result = num1 + num2;
+        result = num1 + num2;
 
-    //     Console.WriteLine("{0} + {1} + = {2}", num1, num2, result);
+        Console.WriteLine("{0} + {1} + = {2}", num1, num2, result);
     
-    //     Console.WriteLine("press any key to exit");
-    //     Console.ReadKey();
+        Console.WriteLine("press any key to exit");
+        Console.ReadKey();
 
             
-    //     }
-    //   }
-    // }
+        }
+      }
+    }
